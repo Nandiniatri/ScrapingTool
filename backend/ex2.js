22a9e1c16a2aa64a6d741d0b752b7f104eaaf5c2
Nandiniatri <atrinandini8@gmail.com>
2025-11-04 14:29:25 +0530

    change code

--- backend/ex2.js
+++ backend/ex2.js
@@ -4,14 +4,15 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-const PORT = 4000;
+// const PORT = 4000;
 
 const users = [
-    {id:1 , title:"Nandini Atri"}
+    { id: 1, title: "Nandini Atri" },
+    { id: 2, title: "Backend Connected Successfully 🚀" }
 ]
 
-app.get("/api/users" , (req , res) => {
+app.get("/api/users", (req, res) => {
     res.json(users);
 })
 
-app.listen(PORT , () => console.log(`server running on https://localhost:${PORT}`));
+app.listen(4000, () => console.log('server running on https://localhost:4000'));
