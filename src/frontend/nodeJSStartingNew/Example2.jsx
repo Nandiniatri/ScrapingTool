4830a37c322042a6c4dfde57113f138ef13eb043
Nandiniatri <atrinandini8@gmail.com>
2025-11-04 14:31:50 +0530

    add data in frontend and make a connection for that

--- src/frontend/nodeJSStartingNew/Example2.jsx
+++ src/frontend/nodeJSStartingNew/Example2.jsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from "react";
 
 const Example2 = () => {
-    const [data , setData] = useState([]);
+    const [data, setData] = useState([]);
 
-    const fetchUsers = async() => {
-        const response = await fetch("https://localhost:4000/api/users");
+    const fetchUsers = async () => {
+        const response = await fetch("/api/users");
         const result = await response.json();
         setData(result);
     }
 
     useEffect(() => {
         fetchUsers();
-    },[])
+    }, [])
 
     return (
-        <>
-            {data.map((item) => {
-                return (
-                    <h1>{item.title}</h1>
-                )
-            })}
-        </>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h2>👋 Frontend Connected to Backend</h2>
+            {data.map(user => (
+                <h3 key={user.id}>{user.title}</h3>
+            ))}
+        </div>
     )
 }
 
