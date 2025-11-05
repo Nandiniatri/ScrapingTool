0ffa3b7a0505df84856d48a7014caa1d3853cbc7
Nandiniatri <atrinandini8@gmail.com>
2025-11-05 11:42:56 +0530

    add supabase keys

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://rlezesnrywymoobhejat.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsZXplc25yeXd5bW9vYmhlamF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5MzM0OTcsImV4cCI6MjA3MTUwOTQ5N30.oEsxb5vmeKUHRvG7wu87r41jhvFQLCqzuaOy2e3xQx4';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
