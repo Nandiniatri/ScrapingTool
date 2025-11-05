152329e8c28c0a0e145ad2507b840e9df5dbeafb
Nandiniatri <atrinandini8@gmail.com>
2025-11-05 11:47:08 +0530

    add supabse code

---
+++ backend/withSupabase.js
@@ -0,0 +1,14 @@
+import { supabase } from './supabaseClient.js'
+
+async function addUser(name, email) {
+  const { data, error } = await supabase
+    .from('users')
+    .insert({ name: name, email: email })
+  
+  if (error) console.error('Insert error:', error)
+  else console.log('Inserted user:', data)
+}
+
+// Test call
+addUser('Anita', 'anita@example.com')
+
