30530cb1414bae8daeddf514cddf4f57c9796c72
Nandiniatri <atrinandini8@gmail.com>
2025-11-06 11:12:45 +0530

    make a input fielf

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Users1 from './frontend/fetchDataWithJsonInBackend'
 import BooksData from './frontend/JeansData'
 import Example1 from './frontend/nodeJSStartingNew/Example1'
 import Example2 from './frontend/nodeJSStartingNew/Example2'
+import InputField from './frontend/nodeJSStartingNew/inputField'
 import WithSupabaseFetchDataComponent from './frontend/nodeJSStartingNew/WithSupabaseFetchData'
 import Users from './frontend/simpleBackendDataFetch'
 
@@ -17,7 +18,8 @@ function App() {
       {/* <BooksData /> */}
       {/* <Example1 /> */}
       {/* <Example2 /> */}
-      <WithSupabaseFetchDataComponent />
+      {/* <WithSupabaseFetchDataComponent /> */}
+      <InputField />
     </>
   )
 }
