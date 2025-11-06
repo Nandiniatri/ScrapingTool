43093002f7ec9dd91af373e32c56f0fe25860ecb
Nandiniatri <atrinandini8@gmail.com>
2025-11-06 11:15:13 +0530

    make a inputFieldCode

--- src/frontend/nodeJSStartingNew/inputField.jsx
+++ src/frontend/nodeJSStartingNew/inputField.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 const InputField = () => {
+    const [inputValueSave , setInputValueSave] = useState('');
+
+    const handleText = (e) => {
+        console.log(e.target.value);
+        
+        // setInputValueSave()
+    }
+
     return (
         <>
+            <input type="text" onChange={(e) => handleText(e)} value={inputValueSave}/>
+
         </>
     )
 }
